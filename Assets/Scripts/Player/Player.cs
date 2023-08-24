fa4ed133baa44321aba8f9a235d93f75bda9f77f
MetaDrow <106996152+MetaDrow@users.noreply.github.com>
2023-08-24 19:06:11 +0300

    add & setup player input

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] PhotonView _view;
+    [SerializeField] CharacterController _controller;
+
+    [Header("Movement Parameters")]
+    protected internal Vector2 _input;
+    private Vector3 _velocity;
+    [SerializeField, Range(0, 50)] float _maxSpeed;
 
     void Start()
     {
-
+        _controller = GetComponent<CharacterController>();
+        _view = GetComponent<PhotonView>();
     }
 
-
-    void Update()
+    void FixedUpdate()
     {
+        if (_view.IsMine)
+        {
+            Move(_input);
+        }
+    }
 
+    void Move(Vector2 input)
+    {
+        _velocity = input * _maxSpeed;
+        _controller.Move(_velocity * Time.fixedDeltaTime);
     }
 }
